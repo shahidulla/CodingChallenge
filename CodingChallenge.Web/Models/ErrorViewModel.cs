f4b4e48c85460add85c2999d61a55496d07d6932
Md Shahidulla <mshahidullah@plsemail.com>
2021-01-26 07:41:43 -0600

    Add project files.

---
+++ CodingChallenge.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodingChallenge.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
